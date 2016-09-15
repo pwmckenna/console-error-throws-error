a46f79f926e10ca08e027886b34623a10a364c24
Patrick Williams <patrickw@porch.com>
2016-09-15 14:13:33 -0700

    log before throwing

--- toggle.js
+++ toggle.js
@@ -2,7 +2,10 @@ var error;
 module.exports = {
     on: function () {
         error = console.error;
-        console.error = function (msg) { throw (msg instanceof Error ? msg : new Error(msg)); }
+        console.error = function (msg) {
+            error.apply(console, arguments);
+            throw (msg instanceof Error ? msg : new Error(msg));
+        }
     },
     off: function () {
         console.error = error;
